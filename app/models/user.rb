fe25b435470349f6efc5eb27c5b87c1af65b48bb
YOUR NAME <tmhennis@gmail.com>
2016-10-13 18:24:48 +0200

    added validations and cascade deletes to products

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_one :profile
-  has_many :products
+  has_many :products, dependent: :destroy
 
   def has_profile?
     profile.present?
