22441554d2211feb58c15936cb10408d46d85521
adrianabm <adrianabm@gmail.com>
2016-10-13 19:31:06 +0200

    add images

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012143256) do
+ActiveRecord::Schema.define(version: 20161013165056) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.integer  "product_id"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_photos_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
